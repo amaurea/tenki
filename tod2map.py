984db4dc21a46f2cc0d98fb0769ad3231b207f81
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2014-09-21 09:55:23 -0400

    Made tod2map use the new log and bench system.

--- tod2map.py
+++ tod2map.py
@@ -1,6 +1,6 @@
 import numpy as np, time, h5py, copy, argparse, os, mpi4py.MPI, sys, pipes, shutil
 from enlib import enmap, utils, pmat, fft, config, array_ops, map_equation, nmat, errors
-from enlib import scansim
+from enlib import log, bench
 from enlib.cg import CG
 from enact import data, nmat_measure, filedb
 
@@ -9,6 +9,7 @@ config.default("map_bits", 32, "Bit-depth to use for maps and TOD")
 config.default("downsample", 1, "Factor with which to downsample the TOD")
 config.default("map_precon", "bin", "Preconditioner to use for map-making")
 config.default("map_cg_nmax", 1000, "Max number of CG steps to perform in map-making")
+config.default("verbosity", 1, "Verbosity for output. Higher means more verbose. 0 outputs only errors etc. 1 outputs INFO-level and 2 outputs DEBUG-level messages.")
 
 parser = config.ArgumentParser(os.environ["HOME"] + "/.enkirc")
 parser.add_argument("filelist")
@@ -48,8 +49,16 @@ if myid == 0:
 		for id in filelist:
 			f.write("%s\n" % id)
 	shutil.copyfile(config.get("filedb"), args.odir + "/filedb.txt")
+# Set up logging
+utils.mkdir(args.odir + "/log")
+logfile   = args.odir + "/log/log%03d.txt" % myid
+log_level = log.verbosity2level(config.get("verbosity"))
+L = log.init(level=log_level, file=logfile, rank=myid)
+# And benchmarking
+utils.mkdir(args.odir + "/bench")
+benchfile = args.odir + "/bench/bench%03d.txt" % myid
 
-if myid == 0: print "Reading scans"
+L.info("Reading scans")
 tmpinds    = np.arange(len(filelist))[myid::nproc]
 myscans, myinds  = [], []
 for ind in tmpinds:
@@ -58,22 +67,22 @@ for ind in tmpinds:
 		if args.ndet > 0: d = d[:args.ndet]
 		myscans.append(d)
 		myinds.append(ind)
-		print "Read %s" % filelist[ind]
+		L.debug("Read %s" % filelist[ind])
 	except errors.DataMissing as e:
-		print "Skipped %s (%s)" % (filelist[ind], e.message)
-		pass
+		L.debug("Skipped %s (%s)" % (filelist[ind], e.message))
 
 nread = comm.allreduce(len(myscans))
-if myid == 0: print "Found %d tods" % nread
+L.info("Found %d tods" % nread)
 if nread == 0:
-	if myid == 0: print "Giving up"
+	L.info("Giving up")
 	sys.exit(1)
 
-if myid == 0: print "Building equation system"
+L.info("Building equation system")
 eq  = map_equation.LinearSystemMap(myscans, area, precon=precon)
 eq.write(args.odir)
+bench.stats.write(benchfile)
 
-if myid == 0: print "Computing approximate map"
+L.info("Computing approximate map")
 x  = eq.M(eq.b)
 bmap = eq.dof.unzip(x)[0]
 if myid == 0: enmap.write_map(args.odir + "/bin.fits", bmap)
@@ -81,15 +90,17 @@ if myid == 0: enmap.write_map(args.odir + "/bin.fits", bmap)
 def solve_cg(eq, nmax=1000, ofmt=None, dump_interval=10):
 	cg = CG(eq.A, eq.b, M=eq.M, dot=eq.dof.dot)
 	while cg.i < nmax:
-		t1 = time.time()
-		cg.step()
-		t2 = time.time()
-		if myid == 0: print "%5d %15.7e %6.3f %6.3f" % (cg.i, cg.err, t2-t1, (t2-t1)/len(eq.scans))
+		with bench.mark("cg_step"):
+			cg.step()
+		dt = bench.stats["cg_step"]["time"].last
+		L.info("%5d %15.7e %6.3f %6.3f" % (cg.i, cg.err, dt, dt/len(eq.scans)))
 		xmap, xjunk = eq.dof.unzip(cg.x)
 		if ofmt and cg.i % dump_interval == 0 and myid == 0:
 			enmap.write_map(ofmt % cg.i, eq.dof.unzip(cg.x)[0])
+		# Output benchmarking information
+		bench.stats.write(benchfile)
 	return cg.x
 
 if nmax > 0:
-	if myid == 0: print "Solving equation"
+	L.info("Solving equation")
 	x = solve_cg(eq, nmax=nmax, ofmt=args.odir + "/dump%04d.fits", dump_interval=args.dump)

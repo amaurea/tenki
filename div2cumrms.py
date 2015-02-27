8f9fc4769b27ca37850d70e9d98e50653abfad4b
Sigurd Næss <sigurdkn@gmail.com>
2015-02-27 11:16:09 -0500

    div2cumrms actually works now

--- div2cumrms.py
+++ div2cumrms.py
@@ -7,10 +7,12 @@ args = parser.parse_args()
 
 div = enmap.read_map(args.div)
 div = div.reshape((-1,)+div.shape[-2:])[0]
-pix_area = div.area()*(180*60/np.pi)**2
+pix_area = div.area()/div.size*(180*60/np.pi)**2
 
 rms  = (pix_area/div)**0.5
 rms  = np.array(np.sort(rms, axis=None))
 area = np.arange(rms.size)*pix_area/3600
+mask = np.isfinite(rms)
+rms, area = rms[mask], area[mask]
 
-np.savetxt("/dev/stdout/", np.array([area[::args.thin],rms[::args.thin]]).T, fmt="%9.3f %15.7e")
+np.savetxt("/dev/stdout", np.array([area[::args.thin],rms[::args.thin]]).T, fmt="%9.3f %15.4f")

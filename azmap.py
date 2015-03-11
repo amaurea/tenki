facc814dd00c34fd1134ddd5ed68b99bb5f3a5a5
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2015-03-11 20:36:04 +0200

    Azimuth phase output in azbin, as well as gapfilling in poltod to make power spectra easier to interpet. azbin is very messy - not very reusable.

--- azmap.py
+++ azmap.py
@@ -3,7 +3,8 @@ that relative azimuth is all that matters, so time, elevation and azimuth center
 can be ignored."""
 import numpy as np, mpi4py.MPI, sys, os, time, h5py
 from enact import filedb, data
-from enlib import enmap, config, utils, array_ops, fft, errors
+from enlib import enmap, config, utils, array_ops, fft, errors, gapfill, rangelist
+from scipy.interpolate import UnivariateSpline
 
 parser = config.ArgumentParser(os.environ["HOME"] + "/.enkirc")
 parser.add_argument("selector")
@@ -26,20 +27,42 @@ def highpass(tod, f, srate=400):
 	return tod
 
 # cumulative az bin
-crhs = np.zeros([3,nbin])
-cdiv = np.zeros([3,3,nbin])
-trhs, tdiv = crhs.copy(), cdiv.copy()
+my_arhs, tot_arhs = np.zeros([2,3,nbin])
+my_adiv, tot_adiv = np.zeros([2,3,3,nbin])
+my_prhs, tot_prhs = np.zeros([2,3,nbin])
+my_pdiv, tot_pdiv = np.zeros([2,3,3,nbin])
+my_acc_arhs, tot_acc_arhs = np.zeros([2,nbin])
+my_acc_adiv, tot_acc_adiv = np.zeros([2,nbin])
+my_acc_prhs, tot_acc_prhs = np.zeros([2,nbin])
+my_acc_pdiv, tot_acc_pdiv = np.zeros([2,nbin])
 
 def output_cum(si):
-	comm.Allreduce(crhs, trhs)
-	comm.Allreduce(cdiv, tdiv)
+	comm.Allreduce(my_arhs, tot_arhs)
+	comm.Allreduce(my_adiv, tot_adiv)
+	comm.Allreduce(my_prhs, tot_prhs)
+	comm.Allreduce(my_pdiv, tot_pdiv)
+	comm.Allreduce(my_acc_arhs, tot_acc_arhs)
+	comm.Allreduce(my_acc_adiv, tot_acc_adiv)
+	comm.Allreduce(my_acc_prhs, tot_acc_prhs)
+	comm.Allreduce(my_acc_pdiv, tot_acc_pdiv)
 	if comm.rank == 0:
-		tsig  = array_ops.solve_multi(tdiv, trhs, axes=[0,1])
+		tot_asig  = array_ops.solve_multi(tot_adiv, tot_arhs, axes=[0,1])
+		tot_psig  = array_ops.solve_multi(tot_pdiv, tot_prhs, axes=[0,1])
+		tot_acc_asig = tot_acc_arhs/tot_acc_adiv
+		tot_acc_psig = tot_acc_prhs/tot_acc_pdiv
 		with h5py.File(args.odir + "/cum%04d.hdf" % si, "w") as hfile:
-			hfile["crhs"] = trhs
-			hfile["cdiv"] = tdiv
-			hfile["csig"] = tsig
-
+			hfile["arhs"] = tot_arhs
+			hfile["adiv"] = tot_adiv
+			hfile["asig"] = tot_asig
+			hfile["prhs"] = tot_prhs
+			hfile["pdiv"] = tot_pdiv
+			hfile["psig"] = tot_psig
+			hfile["acc/arhs"] = tot_acc_arhs
+			hfile["acc/adiv"] = tot_acc_adiv
+			hfile["acc/asig"] = tot_acc_asig
+			hfile["acc/prhs"] = tot_acc_prhs
+			hfile["acc/pdiv"] = tot_acc_pdiv
+			hfile["acc/psig"] = tot_acc_psig
 for si in range(comm.rank, len(ids), comm.size):
 	entry = filedb.data[ids[si]]
 	print "Reading %s" % entry.id
@@ -55,38 +78,87 @@ for si in range(comm.rank, len(ids), comm.size):
 	# Estimate white noise level (rough)
 	rms = white_est(d.tod[:,:50000])
 	weight = (1-d.cut.to_mask())/rms[:,None]**2
+
+	if args.filter: d.tod = highpass(d.tod, args.filter)
+
 	# Project whole time-stream to TQU
 	comps = np.zeros([3,ndet])
 	comps[0] = 1
 	comps[1] = np.cos(+2*d.polangle)
 	comps[2] = np.sin(-2*d.polangle)
 	polrhs = comps.dot(d.tod*weight)
-	if args.filter:
-		polrhs = highpass(polrhs, args.filter)
 	poldiv = np.einsum("ad,bd,di->abi",comps,comps,weight)
 	poltod = array_ops.solve_multi(poldiv,polrhs,axes=[0,1])
-	# Bin by az
+	del weight
 	print "Computing az bin"
 	az    = d.boresight[1]
-	abox  = np.array([np.min(az),np.max(az)])
-	pix   = np.minimum(np.floor((az-abox[0])/(abox[1]-abox[0])*nbin).astype(int),nbin-1)
-	arhs  = np.array([np.bincount(pix, r, minlength=nbin) for r in polrhs])
-	adiv  = np.zeros([3,3,nbin])
-	for i in range(3):
-		for j in range(3):
-			adiv[i,j] = np.bincount(pix, poldiv[i,j], minlength=nbin)
-	asig  = array_ops.solve_multi(adiv, arhs, axes=[0,1])
+	def build_bins(a, nbin):
+		box = np.array([np.min(a),np.max(a)])
+		return np.minimum(np.floor((a-box[0])/(box[1]-box[0])*nbin).astype(int),nbin-1)
+	def bin_by_pix(a, pix, nbin):
+		a = np.asarray(a)
+		if a.ndim == 0: a = np.full(len(pix), a)
+		fa = a.reshape(-1,a.shape[-1])
+		fo = np.zeros([fa.shape[0],nbin])
+		for i in range(len(fa)):
+			fo[i] = np.bincount(pix, fa[i], minlength=nbin)
+		return fo.reshape(a.shape[:-1]+(nbin,))
+	# Gapfill poltod in regions with far too few hits, to
+	# avoid messing up the poltod power spectrum
+	mask = poldiv[0,0] < np.mean(poldiv[0,0])*0.1
+	for i in range(poltod.shape[0]):
+		poltod[i] = gapfill.gapfill_copy(poltod[i], rangelist.Rangelist(mask))
+
+	# Calc phase which is equal to az while az velocity is positive
+	# and 2*max(az) - az while az velocity is negative
+	x = np.arange(len(az))
+	az_spline = UnivariateSpline(x, az, s=1e-4)
+	daz  = az_spline.derivative(1)(x)
+	ddaz = az_spline.derivative(2)(x)
+	phase = az.copy()
+	phase[daz<0] = 2*np.max(az)-az[daz<0]
+	# Bin by az
+	apix = build_bins(az, nbin)
+	arhs = bin_by_pix(polrhs, apix, nbin)
+	adiv = bin_by_pix(poldiv, apix, nbin)
+	asig = array_ops.solve_multi(adiv, arhs, axes=[0,1])
+	aacc_rhs = bin_by_pix(ddaz, apix, nbin)
+	aacc_div = bin_by_pix(1, apix, nbin)
+	aacc_sig = aacc_rhs/aacc_div
+	# Bin by phase
+	ppix = build_bins(phase, nbin)
+	prhs = bin_by_pix(polrhs, ppix, nbin)
+	pdiv = bin_by_pix(poldiv, ppix, nbin)
+	psig = array_ops.solve_multi(pdiv, prhs, axes=[0,1])
+	pacc_rhs = bin_by_pix(ddaz, ppix, nbin)
+	pacc_div = bin_by_pix(1, ppix, nbin)
+	pacc_sig = pacc_rhs/pacc_div
 	with h5py.File(args.odir + "/"+ entry.id, "w") as hfile:
 		hfile["poltod"] = poltod
 		hfile["polrhs"] = polrhs
 		hfile["poldiv"] = poldiv
+		hfile["az"]    = az
 		hfile["asig"] = asig
 		hfile["arhs"] = arhs
 		hfile["adiv"] = adiv
-		hfile["az"] = az
-		hfile["pix"] = pix
+		hfile["aacc_sig"] = aacc_sig
+		hfile["aacc_rhs"] = aacc_rhs
+		hfile["aacc_div"] = aacc_div
+		hfile["phase"] = phase
+		hfile["psig"] = psig
+		hfile["prhs"] = prhs
+		hfile["pdiv"] = pdiv
+		hfile["pacc_sig"] = pacc_sig
+		hfile["pacc_rhs"] = pacc_rhs
+		hfile["pacc_div"] = pacc_div
 	# Update cumulative
-	crhs += arhs
-	cdiv += adiv
+	my_arhs += arhs
+	my_adiv += adiv
+	my_prhs += prhs
+	my_pdiv += pdiv
+	my_acc_arhs += aacc_rhs
+	my_acc_adiv += aacc_div
+	my_acc_prhs += pacc_rhs
+	my_acc_pdiv += pacc_div
 
 	output_cum(si)

c0fba8c5a307f946b00ea5b9b6ede1e7ddf13ea2
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2015-04-13 10:45:39 -0400

    Committing to move to laptop

--- tod2nmat.py
+++ tod2nmat.py
@@ -1,6 +1,6 @@
-import numpy as np, argparse, time, os, zipfile
+import numpy as np, argparse, time, os, zipfile, h5py
 from mpi4py import MPI
-from enlib import utils, fft, nmat, errors, config, bench
+from enlib import utils, fft, nmat, errors, config, bench, array_ops
 from enact import filedb, todinfo, data, nmat_measure
 
 config.default("filedb", "filedb.txt", "File describing the location of the TOD and their metadata")
@@ -10,6 +10,8 @@ parser.add_argument("filelist")
 parser.add_argument("odir")
 parser.add_argument("-m", "--model", default="jon")
 parser.add_argument("-c", "--resume", action="store_true")
+parser.add_argument("-s", "--spikecut", type=int, default=1)
+parser.add_argument("-C", "--covtest", action="store_true")
 args = parser.parse_args()
 
 comm = MPI.COMM_WORLD
@@ -31,22 +33,53 @@ for i in myinds:
 	if os.path.isfile(ofile) and args.resume: continue
 	t=[]; t.append(time.time())
 	try:
-		d  = data.read(entry, ["gain","tconst","cut","tod","boresight"])   ; t.append(time.time())
-		d  = data.calibrate(d)                                             ; t.append(time.time())
-		ft = fft.rfft(d.tod) * d.tod.shape[1]**-0.5                          ; t.append(time.time())
+		fields = ["gain","tconst","cut","tod","boresight"]
+		if args.spikecut: fields.append("spikes")
+		d  = data.read(entry, fields)                ; t.append(time.time())
+		d  = data.calibrate(d)                       ; t.append(time.time())
+		ft = fft.rfft(d.tod) * d.tod.shape[1]**-0.5  ; t.append(time.time())
+		spikes = d.spikes[:2].T if args.spikecut else None
 		if model == "old":
 			noise = nmat_measure.detvecs_old(ft, d.srate, d.dets)
 		elif model == "jon":
 			di = np.where(d.dets==20)[0]
-			noise = nmat_measure.detvecs_jon(ft, d.srate, d.dets, shared)
+			noise = nmat_measure.detvecs_jon(ft, d.srate, d.dets, shared, cut_bins=spikes)
 		elif model == "simple":
 			noise = nmat_measure.detvecs_simple(ft, d.srate, d.dets)
+		elif model == "joint":
+			noise = nmat_measure.detvecs_joint(ft, d.srate, d.dets, cut_bins=spikes)
 		t.append(time.time())
-		nmat.write_nmat("%s/%s.hdf" % (args.odir, id), noise)                ; t.append(time.time())
+		with h5py.File("%s/%s.hdf" % (args.odir, id),"w") as hfile:
+			nmat.write_nmat(hfile, noise)                ; t.append(time.time())
+			if args.covtest:
+				# Measure full cov per bin
+				ndet = d.tod.shape[0]
+				bins = np.minimum((noise.bins*ft.shape[1]/noise.bins[-1,1]).astype(int),ft.shape[1]-1)
+				nbin = len(bins)
+				cov_full = np.zeros([nbin,ndet,ndet])
+				for bi, b in enumerate(bins):
+					print "A", bi, b, np.mean(np.abs(ft[0,b[0]:b[1]])**2)**0.5/20
+					cov_full[bi]  = nmat_measure.measure_cov(ft[:,b[0]:b[1]])
+				cov_model= noise.covs
+				# Compute total noise and correlated noise per detector for the two
+				pow_full_tot  = np.einsum("bii->bi",cov_full)
+				pow_model_tot = np.einsum("bii->bi",cov_model)
+				pow_full_ucorr = 1/np.einsum("bii->bi",array_ops.eigpow(cov_full,-1))
+				pow_model_ucorr = 1/np.einsum("bii->bi",array_ops.eigpow(cov_model,-1))
+				corr_full = cov_full/(pow_full_tot[:,:,None]*pow_full_tot[:,None,:])**0.5
+				corr_model = cov_model/(pow_model_tot[:,:,None]*pow_model_tot[:,None,:])**0.5
+				# And write
+				hfile["full/corr"]  = corr_full
+				hfile["full/tpow"] = pow_full_tot
+				hfile["full/upow"] = pow_full_ucorr
+				hfile["model/corr"]  = corr_model
+				hfile["model/tpow"] = pow_model_tot
+				hfile["model/upow"] = pow_model_ucorr
 		t = np.array(t)
 		dt= t[1:]-t[:-1]
 	except (errors.DataMissing, ValueError, AssertionError, np.linalg.LinAlgError) as e:
 		print "%3d/%d %25s skip (%s)" % (i+1,n,id, e.message)
+		raise
 		continue
 	except zipfile.BadZipfile:
 		print "%d/%d %25s bad zip" % (i+1,n,id)
--- totspec.py
+++ totspec.py
@@ -6,6 +6,7 @@ parser = config.ArgumentParser(os.environ["HOME"] + "/.enkirc")
 parser.add_argument("sel")
 parser.add_argument("ofile")
 parser.add_argument("-N", type=int, default=10000)
+parser.add_argument("-w", "--weight", action="store_true")
 args = parser.parse_args()
 
 comm = MPI.COMM_WORLD
@@ -13,9 +14,13 @@ srate = 400.
 fmax  = srate/2
 ndet = 33*32
 
+# Use power at 10 Hz to compute weights
+fref = [9.5,10.5]
+
 # Create a global (all tods + all dets) 1-D noise spectrum. Result
 # will probably have to be highpass filtered, but that can be done elsewhere
 myps = np.zeros(args.N)
+mynspec = 0
 
 filedb.init()
 ids = filedb.scans[args.sel].ids
@@ -37,10 +42,17 @@ for si in range(comm.rank, len(ids), comm.size):
 
 	for det, ps_det in zip(d.dets,ps):
 		ps_bin = np.bincount(inds, ps_det, minlength=args.N)/np.bincount(inds, minlength=args.N)
-		myps += ps_bin
+		weight = 1.0
+		if args.weight:
+			iref = [f*n/srate for f in fref]
+			weight = 1.0/np.mean(ps_det[iref[0]:iref[1]])
+		myps += ps_bin*weight
+		mynspec += weight
 
 totps = myps.copy()
 comm.Allreduce(myps, totps)
+totnspec = comm.allreduce(mynspec)
+totps /= totnspec
 totfreq = np.linspace(0, fmax, args.N)
 
 if comm.rank == 0:

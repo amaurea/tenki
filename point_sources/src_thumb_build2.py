f730263899146efad2ee8d0b091d880d5fb6a230
Sigurd Næss <sigurdkn@gmail.com>
2017-10-10 14:47:59 -0400

    Added src_thumb_build and fit 2.

---
+++ point_sources/src_thumb_build2.py
@@ -0,0 +1,281 @@
+# Use joneig filtering to clean the area around each point source position.
+# Treat the remainder as white noise. Make thumbnail maps in horizontal coordinates
+# centered on the fiducial source position. Output as fully self-contained hdf files
+# that can be analyzed by the fitter program.
+import numpy as np, os, time, h5py, warnings
+from astropy.io import fits
+from enlib import utils, config, mpi, errors, sampcut, gapfill, cg
+from enlib import pmat, coordinates, enmap, bench, bunch
+from enact import filedb, actdata, actscan, nmat_measure
+config.default("pmat_accuracy", 10.0, "Factor by which to lower accuracy requirement in pointing interpolation. 1.0 corresponds to 1e-3 pixels and 0.1 arc minute in polangle")
+config.default("pmat_interpol_max_size", 1000000, "Maximum mesh size in pointing interpolation. Worst-case time and memory scale at most proportionally with this.")
+parser = config.ArgumentParser(os.environ["HOME"] + "./enkirc")
+parser.add_argument("srclist")
+parser.add_argument("sel")
+parser.add_argument("odir")
+parser.add_argument("-A", "--minamp",    type=float, default=0)
+parser.add_argument("-b", "--box",       type=str,   default="-10:10,-10:10")
+parser.add_argument("-p", "--pad",       type=float, default=2)
+parser.add_argument("-r", "--res",       type=float, default=0.1)
+parser.add_argument("-s", "--restrict",  type=str,   default=None)
+parser.add_argument("-m", "--minimaps",  action="store_true")
+parser.add_argument("-c", "--cont",      action="store_true")
+parser.add_argument("-C", "--cols",      type=str, default="0:1:2")
+args = parser.parse_args()
+
+# The joneig approach requires the mask to be as small as possible, especially in the scan
+# direction, but the source must be entirely contained inside it. Previous tests have shown
+# that x position can vary between -2' to 2'. If we want 3 sigma margin, then we need to add
+# about 2' to that, for a total of 4' radius and 8' = 0.13 degree diameter. We scan at
+# 1 deg/s or so on the sky, so this corresponds to 7.5 Hz. That's a bit lower than I would prefer,
+# considering that our fknee is usually 3 Hz. On the other hand, for planet mapping a
+# radius of 0.2 deg, corresponding to a diameter of 0.4 deg (24') has worked fine.
+
+# We will make thumbnail maps in source-centered horizontal coordinates, and measure the
+# observed source position there. This offset can be related to the pointing offset, though
+# the analytical form is ugly on the curved sky.
+
+filedb.init()
+ids  = filedb.scans[args.sel]
+comm = mpi.COMM_WORLD
+dtype= np.float32
+nref = 2
+min_accuracy = 1.0 # 1 pixel
+
+utils.mkdir(args.odir)
+src_cols = [int(w) for w in args.cols.split(":")]
+
+# Set up thumbnail geometry
+res  = args.res*utils.arcmin
+pad  = args.pad*utils.arcmin
+box  = np.array([[float(w) for w in dim.split(":")] for dim in args.box.split(",")]).T*utils.arcmin
+box[0] -= pad
+box[1] += pad
+shape, wcs = enmap.geometry(pos=box, res=res, proj="car")
+area = enmap.zeros(shape, wcs, dtype)
+
+def read_srcs(fname, cols=(0,1,2)):
+	if fname.endswith(".fits"):
+		data = fits.open(fname)[1].data
+		return np.array([data.ra*utils.degree,data.dec*utils.degree,data.sn])
+	else:
+		data = np.loadtxt(fname, usecols=cols).T
+		data[:2] *= utils.degree
+		return data
+
+def find_ref_pixs(divs, rcost=1.0, dcost=1.0):
+	"""rcost is cost per pixel away from center
+	dcost is cost per dB change in div value avay from median"""
+	# Find median nonzero div per map
+	ref_val = np.asarray(np.median(np.ma.array(divs, mask=divs==0),(-2,-1)))
+	with utils.nowarn():
+		val_off = 10*(np.log10(divs)-np.log10(ref_val[:,None,None]))
+	pix_map = divs.pixmap()
+	center  = np.array(divs.shape[-2:])/2
+	dist_map= np.sum((pix_map-center[:,None,None])**2,-3)**0.5
+	cost    = dist_map * rcost + np.abs(val_off)*dcost
+	# Sort each by cost. Will be [{y,x},map,order]
+	inds    = np.array(np.unravel_index(np.argsort(cost.reshape(cost.shape[0],-1),1), cost.shape[-2:]))
+	inds    = inds.T
+	# Want to return [order,map,{y,x}]
+	return inds
+
+def measure_corr(pmaps, nmat, divs, tod_work, ref_pixs):
+	ref_pixs = np.array(ref_pixs)
+	tod_work[:] = 0
+	map_work    = enmap.zeros((3,)+divs.shape[-2:], divs.wcs, divs.dtype)
+	corrs       = enmap.zeros(divs.shape, divs.wcs, divs.dtype)
+	for i, pmap in enumerate(pmaps):
+		map_work[:] = 0
+		map_work[0,ref_pixs[i,0],ref_pixs[i,1]] = 1
+		pmap.forward(tod_work, map_work, tmul=1)
+	nmat.apply(tod_work)
+	for i, pmap in enumerate(pmaps):
+		pmap.backward(tod_work, map_work, mmul=0)
+		corrs[i] = map_work[0]
+		norm     = (divs[i,ref_pixs[i,0],ref_pixs[i,1]] * divs[i])**0.5
+		corrs[i,norm>0] /= norm[norm>0]
+		corrs[i] = np.roll(np.roll(corrs[i], -ref_pixs[i,0], -2), -ref_pixs[i,1], -1)
+	# Shift center to corner, so the result is the correlation relative to
+	# corner pixel
+	return corrs
+
+class PmatTot:
+	def __init__(self, scan, area, sys=None):
+		self.pmap  = pmat.PmatMap(scan, area, sys=sys)
+		self.pcut  = pmat.PmatCut(scan)
+		self.err   = self.pmap.err
+	def forward(self, tod, map, tmul=0, mmul=1):
+		junk = np.zeros(self.pcut.njunk, tod.dtype)
+		self.pmap.forward(tod, map, tmul=tmul, mmul=mmul)
+		self.pcut.forward(tod, junk)
+	def backward(self, tod, map, tmul=1, mmul=1):
+		junk = np.zeros(self.pcut.njunk, tod.dtype)
+		self.pcut.backward(tod, junk)
+		self.pmap.backward(tod, map, tmul=tmul, mmul=mmul)
+
+# Get source bounds
+bounds = filedb.scans.select(ids).data["bounds"]
+
+# Load source database
+srcdata  = read_srcs(args.srclist, cols=src_cols)
+# srcpos is [{ra,dec},nsrc] in radians, amps is [nsrc]
+srcpos, amps = srcdata[:2], srcdata[2]
+allowed  = set(range(amps.size))
+allowed &= set(np.where(amps > args.minamp)[0])
+if args.restrict is not None:
+	selected = [int(w) for w in args.restrict.split(",")]
+	allowed &= set(selected)
+
+# And loop through our tods
+for ind in range(comm.rank, len(ids), comm.size):
+	id    = ids[ind]
+	oid   = id.replace(":","_")
+	oname = "%s/%s.fits" % (args.odir, oid)
+	if args.cont and os.path.exists(oname):
+		print "%s is done: skipping" % id
+		continue
+
+	# Check if we hit any of the sources. We first make sure
+	# there's no angle wraps in the bounds, and then move the sources
+	# to the same side of the sky.
+	poly      = bounds[:,:,ind]*utils.degree
+	poly[0]   = utils.rewind(poly[0],poly[0,0])
+	srcpos    = srcpos.copy()
+	srcpos[0] = utils.rewind(srcpos[0], poly[0,0])
+	sids      = np.where(utils.point_in_polygon(srcpos.T, poly.T))[0]
+	sids      = np.array(sorted(list(set(sids)&allowed)))
+	if len(sids) == 0:
+		print "%s has 0 srcs: skipping" % id
+		continue
+	nsrc = len(sids)
+	print "%s has %d srcs: %s" % (id,nsrc,", ".join(["%d (%.1f)" % (i,a) for i,a in zip(sids,amps[sids])]))
+
+	entry = filedb.data[id]
+	try:
+		with bench.mark("read"):
+			d = actdata.read(entry)
+		with bench.mark("calib"):
+			d = actdata.calibrate(d, exclude=["autocut"])
+		if d.ndet < 2 or d.nsamp < 1: raise errors.DataMissing("no data in tod")
+	except errors.DataMissing as e:
+		print "%s skipped: %s" % (id, e.message)
+		continue
+	tod = d.tod.astype(dtype)
+	del d.tod
+
+	# We need a white noise estimate per detector. How should we get this?
+	# 1. Use atmosphere-removed samples. But these have the source in them.
+	#    But an individual detector is probably pretty noise dominated, so it might work.
+	# 2. Use standard noise model to estimate the noise level. A bit slow, but not that bad.
+	# 3. Use the running difference to estimate it. This is simple, but may place too
+	#    much emphasis on the high-frequency part.
+	# I'll go for #2 now, since it's tried and true.
+	scan = actscan.ACTScan(entry, d=d)
+	nmat = scan.noise.update(tod, d.srate)
+
+	# Set up the local source-centered coordinate system for each source.
+	# This coordinate system specification is pretty clunky and string-based, sadly :/
+	syss = ["hor:%.6f_%.6f:cel/0_0:hor" % tuple(srcpos[:,sid]/utils.degree) for sid in sids]
+
+	# We now need to subtract the atmosphere behind every source. For this we need to
+	# set up cuts corresponding to those samples. The easiest way to do this is via
+	# PmatMat.
+	pmaps = []
+	for sys in syss:
+		pmap = PmatTot(scan, area, sys=sys)
+		err  = np.max(pmap.err[:2])
+		if err > min_accuracy:
+			print "Pointing model failed for '%s' with error %f. Skipping" % (id, err)
+			break
+	if len(pmaps) < len(syss): continue
+
+	## Use joneig gapfilling to remove the atmosphere. This will zero out samples outside our
+	## regions of interest. This does not appear to work as well as I had hoped. The noise is
+	## quite strongly non-white even after this step. This probably makes this approach unworkable.
+	#tod -= gapfill.gapfill_joneig(tod, tot_cut, inplace=False)
+
+	# Ok, we can now build the thumb maps. Will use a constant correlation noise model:
+	# M = hD C hD, where hD = sqrt(div) and C is fourier-diagonal. Compute C via:
+	# set center pixel to 1, P'N"P. This gives us a row of M, the inverse pixel-pixel covmat.
+	# Divide out the uncorrelated part: M_ij/d_i/d_j, where i is the pixel we set to 1, and
+	# j is each pixel in the map. This should make the central pixel approximately 1.
+	# Using this approximate model, we can solve for the map as:
+	# m = hD" C" hD" rhs
+	# and the likelihood is then
+	# lik = (m-Pa)hD C hD(m-Pa)
+	# hD m = C" hD" rhs
+	# lik = (w - hC hD Pa)**2, w = hC hD hD" C" hD" rhs = hC" hD" rhs
+
+	rhss  = enmap.zeros((nsrc,)+shape, wcs, dtype)
+	divs  = enmap.zeros((nsrc,)+shape, wcs, dtype)
+	work  = enmap.zeros((3,)   +shape, wcs, dtype)
+	nmat.apply(tod)
+	for i, pmap in enumerate(pmaps):
+		pmaps[i].backward(tod, work, mmul=0)
+		rhss[i] += work[0]
+	tod[:] = 1.0
+	nmat.white(tod)
+	for i, pmap in enumerate(pmaps):
+		pmaps[i].backward(tod, work, mmul=0)
+		divs[i] += work[0]
+	# Mask unexposed sources
+	hit = np.sum(divs>0,(1,2)) > divs.shape[1]*divs.shape[2]*0.25
+	if np.sum(hit) == 0:
+		print "%s skipped: %s" % (id, "No sources actually hit")
+		with open(oname + ".empty", "w")  as f:
+			f.write(" ".join(["%d" % h for h in hit])+"\n")
+		continue
+	sids  = sids[hit]
+	nsrc  = len(sids)
+	rhss  = rhss[hit]
+	divs  = divs[hit]
+	pmaps = [p for i,p in enumerate(pmaps) if hit[i]]
+
+	corrs  = enmap.zeros((nsrc,)+shape, wcs, dtype)
+	chits  = np.zeros(nsrc)
+	ref_pixs = find_ref_pixs(divs)
+	for i in range(nref):
+		corr = measure_corr(pmaps, nmat, divs, tod, ref_pixs[i])
+		for i in range(nsrc):
+			if corr[i, 0,0] < 0.1: continue
+			corrs[i] += corr[i]
+			chits[i] += 1
+	if np.any(chits==0):
+		print "%s skipped: failed to measure correlations" % id
+		continue
+	corrs /= chits[:,None,None]
+	del tod
+
+	# Write as enmap + fits table
+	omap = enmap.samewcs([rhss, divs, corrs], rhss)
+	header = omap.wcs.to_header(relax=True)
+	header['NAXIS'] = omap.ndim
+	for i,n in enumerate(omap.shape[::-1]):
+		header['NAXIS%d'%(i+1)] = n
+	header['id'] = id
+	header['off_x'] = d.point_correction[0]/utils.arcmin
+	header['off_y'] = d.point_correction[1]/utils.arcmin
+	header['bore_el']  = np.mean(d.boresight[2])/utils.degree
+	header['bore_az1'] = np.min(d.boresight[1])/utils.degree
+	header['bore_az2'] = np.min(d.boresight[1])/utils.degree
+	header['ctime']    = np.mean(d.boresight[0])
+	map_hdu = fits.PrimaryHDU(omap, header)
+
+	srcinfo = np.zeros(nsrc, [('sid','i'),('ra','f'),('dec','f'),('amp','f')])
+	srcinfo["sid"] = sids
+	srcinfo["ra"]  = srcpos[0,sids]/utils.degree
+	srcinfo["dec"] = srcpos[1,sids]/utils.degree
+	srcinfo["amp"] = amps[sids]
+	src_hdu = fits.BinTableHDU(srcinfo, fits.Header({"EXTNAME","srcinfo"}))
+
+	rms  = (nmat.ivar*d.srate)**-0.5
+	detinfo = np.zeros(d.ndet, [('uid','i'),('rms','f')])
+	detinfo["uid"] = d.dets
+	detinfo["rms"] = rms
+	det_hdu = fits.BinTableHDU(detinfo, fits.Header({"EXTNAME","detinfo"}))
+
+	hdus   = fits.HDUList([map_hdu, src_hdu, det_hdu])
+	with warnings.catch_warnings():
+		warnings.filterwarnings('ignore')
+		hdus.writeto(oname, clobber=True)
---
+++ point_sources/src_thumb_fit2.py
@@ -0,0 +1,337 @@
+import numpy as np, os, time, h5py, astropy.io.fits, sys, argparse, copy, warnings
+from scipy import optimize, stats, ndimage
+from enlib import utils, mpi, fft, enmap, bunch, coordinates
+parser = argparse.ArgumentParser()
+parser.add_argument("ifiles", nargs="+")
+parser.add_argument("odir")
+parser.add_argument("-B", "--beam",    type=str,   default="1.3")
+parser.add_argument("-m", "--method",  type=str,   default="mlg")
+parser.add_argument("-b", "--burnin",  type=int,   default=120)
+parser.add_argument("-t", "--thin",    type=int,   default=3)
+parser.add_argument("-n", "--nsamp",   type=int,   default=200)
+parser.add_argument("-v", "--verbose", action="count", default=0)
+parser.add_argument("-q", "--quiet",   action="count", default=0)
+parser.add_argument("-g", "--grid-res",type=float, default=0.5*utils.arcmin)
+parser.add_argument("-M", "--minimaps",action="store_true")
+parser.add_argument("-c", "--cont-from",type=str,  default=None)
+parser.add_argument("-i", "--individual",action="store_true")
+parser.add_argument("-p", "--prior",   type=str,   default="semifix")
+args = parser.parse_args()
+
+comm = mpi.COMM_WORLD
+utils.mkdir(args.odir)
+verbosity = args.verbose - args.quiet
+
+# Set up beam
+try:
+	sigma  = float(args.beam)*utils.fwhm * utils.arcmin
+	nsigma = 10
+	r      = np.arange(0,nsigma,0.01)*sigma
+	beam   = np.exp(-0.5*(r/sigma)**2)
+except ValueError:
+	r, beam = np.loadtxt(args.beam).T
+	r *= utils.degree
+dr = r[1]
+
+def read_thumb_data(fname):
+	res    = bunch.Bunch()
+	hdus   = astropy.io.fits.open(fname)
+	header = hdus[0].header
+	with warnings.catch_warnings():
+		wcs = enmap.enlib.wcs.WCS(header).sub(2)
+	res.rhs, res.div, res.corr = enmap.fix_endian(enmap.ndmap(hdus[0].data, wcs))
+	res.srcinfo = hdus[1].data
+	res.detinfo = hdus[2].data
+	res.id  = header["id"]
+	res.off = np.array([float(header["off_x"]),float(header["off_y"])])*utils.arcmin
+	for key in ["bore_az1","bore_az2","bore_el"]:
+		res[key] = float(header[key])*utils.degree
+	res.ctime = float(header[ctime])
+	return res
+
+def convolve(map, fmap):
+	return fft.ifft(fft.fft(map, axes=(-2,-1))*fmap,axes=(-2,-1), normalize=True).real
+
+class Likelihood:
+	def __init__(self, data, beam, dr, prior="uniform"):
+		self.data = data
+		# Beam setup
+		self.beam = beam
+		self.beam_pre = utils.interpol_prefilter(beam)
+		self.dr   = dr
+		self.pos = self.data.rhs.posmap()
+		self.box = np.sort(self.data.rhs.box(),1)
+		# Noise setup
+		self.hdiv  = data.div**0.5
+		self.ihdiv = self.hdiv.copy()
+		self.ihdiv[self.ihdiv>0] **= -1
+		# Build fourier-version of correlation
+		self.fcorr  = fft.fft(self.data.corr, axes=(-2,-1))
+		if np.any(self.fcorr==0): raise ValueError("Invalid noise correlations")
+		# Prior
+		self.prior = prior
+		self.i = 0
+		self.post0 = None
+		self.min_profile = 0.2
+	def nmat(self, map): return self.hdiv*convolve(self.hdiv*map,self.fcorr)
+	def calc_profile(self, off):
+		pos = self.pos + off[:,None,None]
+		r   = np.sum(pos**2,0)**0.5
+		pix = r/self.dr
+		return enmap.samewcs(utils.interpol(self.beam_pre, pix[None], prefilter=False, mask_nan=False), pos)
+	def calc_amp(self, profile):
+		# (P'N"P)"P'N"d, P = profile, N" = hdiv corr hdiv and d = map
+		# N"d should be rhs. Is it? N"d = hdiv corr hdiv ihdiv icorr ihdiv rhs = rhs. Good
+		amp_rhs = np.sum(profile*self.data.rhs,(-2,-1))
+		amp_div = np.sum(profile*self.nmat(profile),(-2,-1))
+		amp = amp_rhs/amp_div
+		return amp, amp_div
+	def calc_chisq(self, profile):
+		amp, amp_div = self.calc_amp(profile)
+		return np.sum(amp**2*amp_div)
+	@property
+	def nsrc(self): return len(self.data.rhs)
+	def posterior(self, profile):
+		"""Returns -2log(P), so low values are more likely"""
+		# Avoid solutions where we can't even see the data
+		if np.max(profile) < self.min_profile: return np.inf, np.zeros(self.nsrc), np.zeros(self.nsrc)
+		if   self.prior == "uniform":
+			# int da 1/sqrt(|2piN|) exp(-0.5*(d-Pa)'N"(d-Pa))
+			# int da 1/sqrt(|2piN|) exp(-0.5*[(a-ah)'A"(a-ah) + d'N"d - ah'A"ah])
+			# with A" = P'N"P and ah = AP'N"d. Integrating and taking -2log we get
+			# -ah'A"ah + d'N"d + log|2piN| - log|2piA|
+			# the N terms are constant and can be ignored. Ignoring the A-det term
+			# turns out to be equivalent to a jeffrey's prior, which we will use
+			# here (so calling it "uniform" is misleading). That leavses us with
+			# -ah'A"ah
+			amp, amp_div = self.calc_amp(profile)
+			return -np.sum(amp**2*amp_div), amp, amp_div
+		elif self.prior == "semifix":
+			# int da 1/sqrt(|2piN|) exp(-0.5*(d-Pa)'N"(d-Pa)) delta(a-a0)
+			# = 1/sqrt(|2piN|) exp(-0.5*(d-Pa0)'N"(d-Pa0))
+			# : d'N"d - 2(Pa0)'N"d + (Pa0)'N"(Pa0)
+			# = d'N"d - 2(Pa0)'N"d + a0'A"a0
+			# = d'N"d - 2 a0'A"a + a0'A"a0
+			# Can ignore constant d'N"d term and constant determinant term
+			# What if a0 is wrong by factor q => post too high by q**2 => overconfidence
+			# So this doesn't fully solve the problem.
+			amp, amp_div = self.calc_amp(profile)
+			amp0 = self.data.srcinfo.amp
+			post = np.sum(amp0*amp_div*(amp0-2*amp))
+			return post, amp, amp_div
+		elif self.prior == "range":
+			raise NotImplementedError
+		else:
+			raise ValueError
+	def full(self, off):
+		profile = self.calc_profile(off*utils.arcmin)
+		return self.posterior(profile)
+	def __call__(self, off):
+		off = off*utils.arcmin
+		profile = self.calc_profile(off)
+		post, amp, amp_div = self.posterior(profile)
+		if self.post0 is None and np.isfinite(post): self.post0 = post
+		# Flat sky is good enough for our thumbnail
+		#off_tot = off[::-1] + self.data.off
+		msg = "%5d %8.4f %8.4f" % (self.i, off[0]/utils.arcmin, off[1]/utils.arcmin)
+		for a,da in zip(amp, amp_div):
+			msg += " %7.3f %4.1f" % (a/1e3, a*da**0.5)
+		msg += (" %15.7e" % (self.post0-post) if self.post0 is not None else " inf")
+		print msg
+		self.i += 1
+		return post
+	def diag_plot(self, off):
+		profile = self.calc_profile(off)
+		amp, amp_div = self.calc_amp(profile)
+		model_rhs = self.nmat(profile*amp[:,None,None])
+		omap = enmap.samewcs(np.concatenate([self.data.rhs, model_rhs, self.data.rhs-model_rhs][::-1],-1),profile)
+		return omap
+
+def map_likelihood(likfun, rmax=5*utils.arcmin, res=0.7*utils.arcmin):
+	shape, wcs = enmap.geometry(np.array([[-1,-1],[1,1]])*rmax, res=res, proj="car")
+	map = enmap.zeros(shape, wcs)
+	pos = map.posmap()
+	for y in range(map.shape[0]):
+		for x in range(map.shape[1]):
+			map[y,x] = likfun(pos[:,y,x]/utils.arcmin)
+	return map
+
+def find_minimum(likfun, nrun=10, rmax=3*utils.arcmin):
+	poss = []
+	liks = []
+	for i in range(nrun):
+		x0  = np.random.uniform(-rmax,rmax,size=2)/utils.arcmin
+		x   = optimize.fmin_powell(likfun, x0, disp=0)
+		lik = likfun(x)
+		poss.append(x*utils.arcmin)
+		liks.append(lik)
+	poss = np.asarray(poss)
+	liks = np.asarray(liks)
+	# Use the lowest of these
+	pos_mode = poss[np.argmin(liks)]
+	# We can use the scatter as a diagnostic
+	pos_dev  = np.sum(np.var(poss,0))**0.5
+	print pos_mode/utils.arcmin
+	print pos_dev/utils.arcmin
+	print poss/utils.arcmin
+	print liks
+
+# Sampling large-amplitude fixed templates with monte carlo will very easily get
+# stuck in local minima. This is because a 1 sigma fluctuation in noise on top of
+# N-sigma amplitude template will result in a chisquare contribution of (N+1)**2
+# approx N**2 + 2N, while a -1 sigma fluctuation will gvie (N-1)**2 approx N**2 - 2N.
+# So the expected chisquare flucutations have amplitude 2N, not 1. For high N,
+# this means that metropolis would get stuck forever in the first local minimum it
+# sees. That doesn't mean that the likelihood is wrong, though - just that metropolis
+# can't explore it.
+#
+# We can get around this by using normal nonlinear optimization to find the true
+# minimum, and then sample around that.
+
+class Sampler:
+	def __init__(self, likfun, x0, npoint=3, scatter=1.0*utils.arcmin, stepscale=2.0, nsamp=200, burn_frac=0.5, verbose=False):
+		self.likfun  = likfun
+		self.points  = x0 + np.random.uniform(-scatter,scatter,size=(npoint,2))
+		self.likvals = [likfun.full(p/utils.arcmin) for p in self.points]
+		self.next_points  = self.points.copy()
+		self.next_likvals = list(self.likvals)
+		self.stepscale = stepscale
+		self.nsamp   = nsamp
+		self.burn_frac = burn_frac
+		self.verbose = verbose
+		self.i       = 0
+	@property
+	def npoint(self): return self.points.shape[0]
+	@property
+	def ndim(self): return self.points.shape[1]
+	@property
+	def nsrc(self): return self.likvals[0][1].size
+	def draw_scale(self):
+	# scale pdf: 1/sqrt(z) between 1/a and a. so cdf is:
+	# int_(1/a)^x z**-0.5 da = 2[z**0.5]_(1/a)^x = 2*(x**0.5 - a**-0.5)
+	# normalize: (a**0.5-a**-0.5)**-1 * (x**0.5-a**-0.5)
+	# inverse: ((a**0.5-a**-0.5)*p+a**-0.5)**2
+		a = self.stepscale
+		return (np.random.uniform(0,1)*(a**0.5-a**-0.5)+a**-0.5)**2
+	def draw_sample(self):
+		icurrent = self.i % self.npoint
+		if icurrent == 0:
+			# Start of a new cycle
+			self.points  = self.next_points.copy()
+			self.likvals = list(self.next_likvals)
+		iother   = np.random.randint(self.npoint-1)
+		if iother == icurrent: iother += 1
+		scale    = self.draw_scale()
+		cand_pos = self.points[iother] + scale*(self.points[icurrent]-self.points[iother])
+		cand_lik = self.likfun.full(cand_pos/utils.arcmin)
+		paccept  = scale**(self.ndim-1)*np.exp(0.5*(self.likvals[icurrent][0] - cand_lik[0]))
+		r = np.random.uniform(0,1)
+		#print scale, self.points[icurrent], self.likvals[icurrent][0], cand_pos, cand_lik[0], paccept
+		if r < paccept:
+			self.next_points[icurrent] = cand_pos
+			self.next_likvals[icurrent] = cand_lik
+		if self.verbose:
+			self.print_sample(self.next_points[icurrent], self.next_likvals[icurrent])
+		self.i += 1
+		# Return our sample in the form pos amp adiv lik
+		pos = self.next_points[icurrent]
+		lik, amp, adiv = self.next_likvals[icurrent]
+		return pos, amp, adiv
+	def print_sample(self, pos, likvals):
+		lik, amp, adiv = likvals
+		msg = "%5d %8.4f %8.4f" % (self.i, pos[0]/utils.arcmin, pos[1]/utils.arcmin)
+		for a,da in zip(amp, adiv):
+			msg += " %7.3f %4.1f" % (a/1e3, a*da**0.5)
+		msg += " %15.7e" % lik
+		print msg
+	def build_stats(self):
+		nburn = int(self.nsamp*self.burn_frac)
+		for i in range(nburn):
+			self.draw_sample()
+		poss  = np.zeros([self.nsamp, self.ndim])
+		amps  = np.zeros([self.nsamp, self.nsrc])
+		adivs = np.zeros([self.nsamp, self.nsrc])
+		for i in range(self.nsamp):
+			poss[i], amps[i], adivs[i] = self.draw_sample()
+		pos_mean = np.mean(poss,0)
+		amp_mean = np.mean(amps,0)
+		pos_dev  = np.std(poss,0)
+		# Because we're not fully sampling the amplitude, we can't just do np.std(amps,0)
+		# here. We need to take into account adiv too. Conceptually we should
+		# sample from N(amp,1/adiv) for each amp,adiv, and calculate the stats based
+		# on all those. The mean of that will be mean(amp), but the variance will
+		# be var(amp) + mean(vars).
+		amp_dev  = (np.var(amps,0) + np.mean(1/adivs,0))**0.5
+		return bunch.Bunch(pos=pos_mean, dpos=pos_dev, amp=amp_mean, damp=amp_dev)
+
+def format_stats(stats, data):
+	msg = ""
+	for i in range(len(stats.amp)):
+		dy,  dx  = stats.pos/utils.arcmin
+		dy0, dx0 = data.off/utils.arcmin
+		ddy, ddx = stats.dpos/utils.arcmin
+		sid      = data.srcinfo[i]["sid"]
+		afid     = data.srcinfo[i]["amp"]/1e3
+		amp, damp = stats.amp[i]/1e3, stats.damp[i]/1e3
+		msg += "%s | %8.4f %8.4f %8.4f %8.4f %8.4f %8.4f | %4d %8.4f %8.4f %8.4f %5.2f | %8.3f %8.3f\n" % (
+				data.id, dy, dx, dy0, dx0, ddy, ddx, sid, afid, amp, damp, amp/damp)
+	return msg
+
+# Priors:
+# 1. No prior. Simple, but suffers from parameter volume effects when including too many
+#    sources, leading to overconfident predictions.
+# 2. Fix all amps at fiducial. Suboptimal, but may be a good way to find the position.
+# 3. Top-hat prior per source, based on fiducial amp */ some factor. The smaller the factor
+#    the smaller the volume problem and the more optimal the estimate, as long as it
+#    actually contains the real value. Day-time beam variations can cause pretty large
+#    apparent gain fluctuations, but these will be common between the sources in the tod.
+#    After that we're left with intrinsic variability, which can still be substantial.
+#    The 3 strongest sources show night-time amplitude changes of max/min = 2. If we
+#    include the day, the variability is max/min = 4. That is a pretty broad span, especially
+#    considering that the fiducial value might not be in the middle of that range. In the
+#    worst case one might need */ 4, which doesn't do all that much to reduce the volume.
+# 4. Volume division. Given N sources, there are more ways for all of them to be high
+#    than for all of them to be low. This volume factor should be k*sqrt(sum(amp**2))**(n-1)
+#    We can simply divide by that, but that means that if we have a single strong source
+#    and 1000 ignorable tiny ones, then the strong one will be biased low by (n-1)/2 log(amp**2)
+#    or something. Could rescale by expected amplitude before applying this factor? That way
+#    the strong sources won't be more punished than other sources. This seems a bit ad hoc overall.
+#
+# I'm leaning towards #2. Even if you get the amplitudes a bit wrong, the best fit solution
+# should be the same, just with suboptimal errors. The problem is that those errors might
+# be too small, thus repeating the overconfidence problem. That happens if the fiducial amplitude
+# is significantly higher than the actual ones.
+
+for ind in range(comm.rank, len(args.ifiles), comm.size):
+	print ind
+	ifile = args.ifiles[ind]
+	try:
+		thumb_data = read_thumb_data(ifile)
+		lik = Likelihood(thumb_data, beam, dr, prior=args.prior)
+	except Exception as e:
+		sys.stderr.write("Exception for %s: %s\n" % (ifile, e.message))
+		continue
+	print "Processing %s" % thumb_data.id
+	for si, src in enumerate(thumb_data.srcinfo):
+		print "src %4d ra %8.3f dec %8.3f amp %8.3f" % (src["sid"], src["ra"], src["dec"], src["amp"]/1e3)
+	# Build grid
+	if True:
+		likmap  = map_likelihood(lik)
+		pos     = enmap.argmin(likmap)
+		sampler = Sampler(lik, pos, scatter=1e-2*utils.arcmin, verbose=True)
+		stats   = sampler.build_stats()
+		print format_stats(stats, thumb_data)
+	if False:
+		find_minimum(lik)
+	if False:
+		likmap = map_likelihood(lik)
+		enmap.write_map(args.odir + "/grid_%s.fits" % thumb_data.id, likmap)
+	if False:
+		x0  = np.zeros(2)
+		x   = optimize.fmin_powell(lik, x0)
+		off = x*utils.arcmin
+		diag= lik.diag_plot(off)
+		enmap.write_map(args.odir + "/diag_%s.fits" % thumb_data.id, diag)
+	if False:
+		sampler = Sampler(lik, verbose=True)
+		stats   = sampler.build_stats()

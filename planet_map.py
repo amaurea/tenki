Overhaul of auto_coadd. Removes hitcount-beam commutation approximation, and solves a bug that prevented the k-space weighting/filter from doing anything

planet_map.py
@@ -9,8 +9,9 @@ parser.add_argument("area")
 parser.add_argument("sel")
 parser.add_argument("odir")
 parser.add_argument("tag", nargs="?")
-parser.add_argument("-R", "--dist", type=float, default=0.2)
+parser.add_argument("-R", "--dist",    type=float, default=0.2)
 parser.add_argument("-e", "--equator", action="store_true")
+parser.add_argument("-c", "--cont",    action="store_true")
 args = parser.parse_args()
 
 comm = mpi.COMM_WORLD
@@ -35,6 +36,10 @@ for ind in range(comm.rank, len(ids), comm.size):
 	id    = ids[ind]
 	bid   = id.replace(":","_")
 	entry = filedb.data[id]
+	oname = "%s%s_map.fits" % (prefix, bid)
+	if args.cont and os.path.isfile(oname):
+		print "Skipping %s (already done)" % (id)
+		continue
 	# Read the tod as usual
 	try:
 		with bench.show("read"):
@@ -65,7 +70,6 @@ for ind in range(comm.rank, len(ids), comm.size):
 		model= gapfill.gapfill_joneig(tod, planet_cut, inplace=False)
 	# Estimate noise level
 	asens = np.sum(ivar)**-0.5 / d.srate**0.5
-	print asens
 	with bench.show("smooth"):
 		ft   = fft.rfft(model)
 		freq = fft.rfftfreq(model.shape[-1])*d.srate
@@ -108,6 +112,7 @@ for ind in range(comm.rank, len(ids), comm.size):
 	mcent = map[:,c[0]-crad:c[0]+crad,c[1]-crad:c[1]+crad]
 	mcent = enmap.downgrade(mcent, 4)
 	amp   = np.max(mcent)
+	print amp, asens
 	print "%s amp %7.3f asens %7.3f" % (id, amp/1e6, asens)
 	with bench.show("write"):
 		enmap.write_map("%s%s_map.fits" % (prefix, bid), map)

point_sources/src_thumb_fit2.py
@@ -4,23 +4,21 @@ from enlib import utils, mpi, fft, enmap, bunch, coordinates
 parser = argparse.ArgumentParser()
 parser.add_argument("ifiles", nargs="+")
 parser.add_argument("odir")
-parser.add_argument("-B", "--beam",    type=str,   default="1.3")
-parser.add_argument("-m", "--method",  type=str,   default="mlg")
-parser.add_argument("-b", "--burnin",  type=int,   default=120)
-parser.add_argument("-t", "--thin",    type=int,   default=3)
-parser.add_argument("-n", "--nsamp",   type=int,   default=200)
-parser.add_argument("-v", "--verbose", action="count", default=0)
-parser.add_argument("-q", "--quiet",   action="count", default=0)
-parser.add_argument("-g", "--grid-res",type=float, default=0.5*utils.arcmin)
-parser.add_argument("-M", "--minimaps",action="store_true")
-parser.add_argument("-c", "--cont-from",type=str,  default=None)
-parser.add_argument("-i", "--individual",action="store_true")
-parser.add_argument("-p", "--prior",   type=str,   default="semifix")
+parser.add_argument("-B", "--beam",      type=str,   default="1.3")
+parser.add_argument("-m", "--method",    type=str,   default="mlg")
+parser.add_argument("-n", "--nsamp",     type=int,   default=1000)
+parser.add_argument("-v", "--verbose",   action="count", default=0)
+parser.add_argument("-q", "--quiet",     action="count", default=0)
+parser.add_argument("-g", "--grid-res",  type=float, default=0.5)
+parser.add_argument("-M", "--minimaps",  action="store_true")
+parser.add_argument("-p", "--prior",      type=str,   default="semifix")
+parser.add_argument("-N", "--num-walker", type=int,   default=10)
 args = parser.parse_args()
 
 comm = mpi.COMM_WORLD
 utils.mkdir(args.odir)
-verbosity = args.verbose - args.quiet
+verbosity  = args.verbose - args.quiet
+num_walker = args.num_walker
 
 # Set up beam
 try:
@@ -162,6 +160,18 @@ def map_likelihood(likfun, rmax=5*utils.arcmin, res=0.7*utils.arcmin):
 			map[y,x] = likfun(pos[:,y,x]/utils.arcmin)
 	return map
 
+def draw_positions(loglikmap, nwalker):
+	lik  = np.exp(-0.5*(loglikmap-np.min(loglikmap)))
+	lik /= np.sum(lik)
+	cum  = np.cumsum(lik.reshape(-1))
+	ipos = np.searchsorted(cum, np.random.uniform(0,1,size=nwalker))
+	ipos = np.array(np.unravel_index(ipos, lik.shape),dtype=float)
+	# We now have an integer pixel position. Scatter us randomly
+	# inside it
+	ipos += np.random.uniform(-0.5,0.5,size=(2,nwalker))
+	pos   = lik.pix2sky(ipos).T
+	return pos
+
 # Sampling large-amplitude fixed templates with monte carlo will very easily get
 # stuck in local minima. This is because a 1 sigma fluctuation in noise on top of
 # N-sigma amplitude template will result in a chisquare contribution of (N+1)**2
@@ -175,9 +185,9 @@ def map_likelihood(likfun, rmax=5*utils.arcmin, res=0.7*utils.arcmin):
 # minimum, and then sample around that.
 
 class Sampler:
-	def __init__(self, likfun, x0, npoint=3, scatter=1.0*utils.arcmin, stepscale=2.0, nsamp=200, burn_frac=0.5, verbose=False):
+	def __init__(self, likfun, x0, stepscale=2.0, nsamp=200, burn_frac=0.5, verbose=False):
 		self.likfun  = likfun
-		self.points  = x0 + np.random.uniform(-scatter,scatter,size=(npoint,2))
+		self.points  = pos
 		self.likvals = [likfun.full(p/utils.arcmin) for p in self.points]
 		self.next_points  = self.points.copy()
 		self.next_likvals = list(self.likvals)
@@ -319,14 +329,15 @@ for ind in range(comm.rank, len(args.ifiles), comm.size):
 		print "src %4d ra %8.3f dec %8.3f amp %8.3f" % (src["sid"], src["ra"], src["dec"], src["amp"]/1e3)
 
 	# Find out starting point by gridding out the position likelihood coarsly
-	likmap  = map_likelihood(lik)
-	pos     = enmap.argmin(likmap)
+	likmap  = map_likelihood(lik, res=args.grid_res*utils.arcmin)
+	pos     = draw_positions(likmap, num_walker)
+	print pos/utils.arcmin
 	# Sample to get some statistics
-	sampler = Sampler(lik, pos, scatter=1e-2*utils.arcmin, verbose=verbosity>0)
+	sampler = Sampler(lik, pos, nsamp=args.nsamp, verbose=verbosity>0)
 	stats   = sampler.build_stats()
 	# But find the ML point exactly, so we don't have to waste too many samples
 	pos_ml  = optimize.fmin_powell(lik, stats.pos/utils.arcmin, disp=0)*utils.arcmin
-	stats.pos = pos_ml
+	stats.pos_ml = pos_ml
 
 	# Output to stdout and to our indiviudal files
 	msg     = format_stats(stats, thumb_data)

project_map.py
@@ -1,14 +1,25 @@
-import argparse
-from enlib import enmap
+import numpy as np, argparse
+from enlib import enmap, utils
 parser = argparse.ArgumentParser()
 parser.add_argument("imap")
 parser.add_argument("template")
 parser.add_argument("omap")
-parser.add_argument("--order", type=int, default=3)
-parser.add_argument("--mode", type=str, default="constant")
+parser.add_argument("-O", "--order", type=int,   default=3)
+parser.add_argument("-m", "--mode",  type=str,   default="constant")
+parser.add_argument("-M", "--mem",   type=float, default=1e8)
 args = parser.parse_args()
 
-m = enmap.read_map(args.imap)
-t = enmap.read_map(args.template)
-o = enmap.project(m, t.shape, t.wcs, order=args.order, mode=args.mode)
-enmap.write_map(args.omap, o)
+imap  = enmap.read_map(args.imap)
+shape, wcs = enmap.read_map_geometry(args.template)
+omap  = enmap.zeros(shape, wcs, imap.dtype)
+
+blockpix = np.product(shape[:-2])*shape[-1]
+bsize = max(1,utils.nint(args.mem/(blockpix*imap.dtype.itemsize)))
+
+nblock = (shape[-2]+bsize-1)//bsize
+for b in range(nblock):
+	r1, r2 = b*bsize, (b+1)*bsize
+	osub = omap[...,r1:r2,:]
+	omap[...,r1:r2,:] = enmap.project(imap, osub.shape, osub.wcs, order=args.order, mode=args.mode)
+#o = enmap.project(m, t.shape, t.wcs, order=args.order, mode=args.mode)
+enmap.write_map(args.omap, omap)

tod2map2.py
@@ -28,7 +28,7 @@ config.default("signal_uranus_default",  "use=no,type=map,name=uranus,sys=hor:Ur
 config.default("signal_cut_default",   "use=no,type=cut,name=cut,ofmt={name}_{rank:03},output=no,use=yes", "Default parameters for cut (junk) signal")
 config.default("signal_scan_default",  "use=no,type=scan,name=scan,ofmt={name}_{pid:02}_{az0:.0f}_{az1:.0f}_{el:.0f},2way=yes,res=2,tol=0.5", "Default parameters for scan/pickup signal")
 # Default filter parameters
-config.default("filter_scan_default",  "use=no,name=scan,value=1,naz=8,nt=10,nhwp=0,weighted=1,niter=3,sky=yes", "Default parameters for scan/pickup filter")
+config.default("filter_scan_default",  "use=no,name=scan,value=1,daz=3,nt=10,nhwp=0,weighted=1,niter=3,sky=yes", "Default parameters for scan/pickup filter")
 config.default("filter_sub_default",  "use=no,name=sub,value=1,sys=cel,type=map,mul=1,tmul=1,sky=yes", "Default parameters for map subtraction filter")
 config.default("filter_src_default",   "use=no,name=src,value=1,sys=cel,mul=1,sky=yes", "Default parameters for point source subtraction filter")
 config.default("filter_buddy_default",   "use=no,name=buddy,value=1,mul=1,type=map,sys=cel,tmul=1,sky=yes,pertod=0,nstep=200,prec=bin", "Default parameters for map subtraction filter")
@@ -61,6 +61,7 @@ ext   = config.get("map_format")
 tshape= (720,720)
 #tshape= (100,100)
 resume= config.get("resume")
+output_srcmodel = False
 
 filedb.init()
 db = filedb.data
@@ -181,7 +182,9 @@ if read_ntot == 0:
 	sys.exit(1)
 read_ndets= utils.allgatherv([len(scan.dets) for scan in myscans], comm)
 read_nsamp= utils.allgatherv([scan.cut.size-scan.cut.sum() for scan in myscans], comm)
-read_dets = utils.uncat(utils.allgatherv(np.concatenate([scan.dets for scan in myscans]),comm), read_ndets)
+read_dets = utils.uncat(utils.allgatherv(
+	np.concatenate([scan.dets for scan in myscans]) if len(myscans) > 0 else np.zeros(0,int)
+	,comm), read_ndets)
 # Save accept list
 if comm.rank == 0:
 	with open(root + "accept.txt", "w") as f:
@@ -196,7 +199,7 @@ try:
 			ofile.write(("#%29s" + " %15s"*len(autokeys)+"\n") % (("id",)+tuple(autokeys)))
 			for id, acut in zip(read_ids, autocuts):
 				ofile.write(("%30s" + " %7.3f %7.3f"*len(autokeys) + "\n") % ((id,)+tuple(1e-6*acut.reshape(-1))))
-except AttributeError:
+except (AttributeError, IndexError):
 	pass
 # Output sample stats
 if comm.rank == 0:
@@ -370,11 +373,11 @@ for out_ind in range(nouter):
 	filters2= []
 	for param in filter_params:
 		if param["name"] == "scan":
-			naz, nt, mode, niter = int(param["naz"]), int(param["nt"]), int(param["value"]), int(param["niter"])
+			daz, nt, mode, niter = int(param["daz"]), int(param["nt"]), int(param["value"]), int(param["niter"])
 			nhwp = int(param["nhwp"])
 			weighted = int(param["weighted"])
 			if mode == 0: continue
-			filter = mapmaking.FilterPickup(naz=naz, nt=nt, nhwp=nhwp, niter=niter)
+			filter = mapmaking.FilterPickup(daz=daz, nt=nt, nhwp=nhwp, niter=niter)
 			if mode >= 2:
 				for sparam, signal in matching_signals(param, signal_params, signals):
 					if sparam["type"] == "map" or sparam["type"] == "bmap":
@@ -383,7 +386,7 @@ for out_ind in range(nouter):
 						prec_ptp = mapmaking.PreconDmapBinned(signal, signal_cut, myscans, weights=[], noise=False, hits=False)
 					else:
 						raise NotImplementedError("Scan postfiltering for '%s' signals not implemented" % sparam["type"])
-					signal.post.append(mapmaking.PostPickup(myscans, signal, signal_cut, prec_ptp, naz=naz, nt=nt, weighted=weighted>0))
+					signal.post.append(mapmaking.PostPickup(myscans, signal, signal_cut, prec_ptp, daz=daz, nt=nt, weighted=weighted>0))
 		elif param["name"] == "common":
 			mode = int(param["value"])
 			if mode == 0: continue
@@ -443,10 +446,10 @@ for out_ind in range(nouter):
 			else: params = pointsrcs.read(param["params"])
 			params = pointsrcs.src2param(params)
 			params = params.astype(np.float64)
-			print "FIXME: how to handle per-source beams? Forcing to relative for now"
-			params[:,5:7] = 1
-			params[:,7]   = 0
+			# Ignore any per-source beam information. It's not supported.
+			params[:,5:7] = 1; params[:,7]   = 0
 			filter = mapmaking.FilterAddSrcs(myscans, params, sys=param["sys"], mul=-float(param["mul"]))
+			output_srcmodel = True
 		else:
 			raise ValueError("Unrecognized fitler name '%s'" % param["name"])
 		# Add to normal filters of post-noise-model filters based on parameters
@@ -537,6 +540,42 @@ for out_ind in range(nouter):
 
 	L.info("Writing preconditioners")
 	mapmaking.write_precons(signals, root)
+
+	L.info("Computing crosslink map")
+	for param, signal in zip(signal_params, signals):
+		if param["type"] in ["map","bmap","fmap"]:
+			cmap  = enmap.zeros((1,)+signal.area.shape, signal.area.wcs, signal.area.dtype)
+		elif param["type"] in ["dmap"]:
+			geom  = signal.area.geometry.copy()
+			geom.pre = (1,)+geom.pre
+			cmap  = dmap.zeros(geom)
+		else: continue
+		mapmaking.calc_crosslink_map(cmap, signal, signal_cut, myscans, weights)
+		signal.write(root, "crosslink", cmap[0])
+		del cmap
+
+	# Map-space source model computation only works in systems that have a
+	# non-time-dependent transformation to cel. It also assumes that every
+	# tod has the same point source parameters. Here, we will assume that
+	# this applies to the "sky" component, and only this component.
+	if output_srcmodel:
+		for param, signal in zip(signal_params, signals):
+			if param["name"] != "sky": continue
+			L.info("Computing point source map")
+			if param["type"] in ["map","bmap","fmap"]:
+				if comm.rank == 0:
+					srcmap = pointsrcs.sim_srcs(signal.area.shape, signal.area.wcs,
+							myscans[0].pointsrcs, myscans[0].beam, dtype=signal.area.dtype)
+					signal.write(root, "srcs", srcmap)
+					del srcmap
+			elif param["type"] in ["dmap"]:
+				geom  = signal.area.geometry.copy()
+				srcmap  = dmap.zeros(geom)
+				for tile in srcmap.tiles:
+					pointsrcs.sim_srcs(tile.shape, tile.wcs, myscans[0].pointsrcs,
+							myscans[0].beam, omap=tile)
+				signal.write(root, "srcs", srcmap)
+				del srcmap
 
 	L.info("Writing RHS")
 	eqsys.write(root, "rhs", eqsys.b)
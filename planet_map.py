96cda6c71806837561467ca4510422c390bf65e6
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2018-06-28 16:59:13 -0400

    Add simulation stuff to planet_map. Update tenki.enplot to work with new enlib.enplot

--- planet_map.py
+++ planet_map.py
@@ -3,6 +3,8 @@ from enlib import config, pmat, mpi, errors, gapfill, utils, enmap, bench
 from enlib import fft, array_ops
 from enact import filedb, actscan, actdata, cuts, nmat_measure
 
+config.set("pmat_cut_type",  "full")
+
 parser = config.ArgumentParser(os.environ["HOME"]+"./enkirc")
 parser.add_argument("planet")
 parser.add_argument("area")
@@ -12,6 +14,8 @@ parser.add_argument("tag", nargs="?")
 parser.add_argument("-R", "--dist",    type=float, default=0.2)
 parser.add_argument("-e", "--equator", action="store_true")
 parser.add_argument("-c", "--cont",    action="store_true")
+parser.add_argument("--sim",           type=str,   default=None, help="Passing a sel here sets up simulation mode. The simulations will consist of data from the sim sel TODs with the scanning pattern of the real TODs, and with the signal read off from the area map")
+parser.add_argument("--noiseless",      action="store_true", help="Replace signal with simulation instead of adding them. This can be used to get noise free transfer functions")
 args = parser.parse_args()
 
 comm = mpi.COMM_WORLD
@@ -32,6 +36,21 @@ utils.mkdir(args.odir)
 prefix = args.odir + "/"
 if args.tag: prefix += args.tag + "_"
 
+if args.sim:
+	sim_ids = filedb.scans[args.sim][:len(ids)]
+	if area.ndim == 2:
+		tmp = enmap.zeros((ncomp,)+shape, area.wcs, dtype)
+		tmp[0] = area
+		area = tmp
+
+def smooth(tod, srate):
+	ft   = fft.rfft(tod)
+	freq = fft.rfftfreq(tod.shape[-1])*srate
+	flt  = 1/(1+(freq/model_fknee)**model_alpha)
+	ft  *= flt
+	fft.ifft(ft, tod, normalize=True)
+	return tod
+
 for ind in range(comm.rank, len(ids), comm.size):
 	id    = ids[ind]
 	bid   = id.replace(":","_")
@@ -42,8 +61,15 @@ for ind in range(comm.rank, len(ids), comm.size):
 		continue
 	# Read the tod as usual
 	try:
-		with bench.show("read"):
-			d = actdata.read(entry)
+		if not args.sim:
+			with bench.show("read"):
+				d = actdata.read(entry)
+		else:
+			sim_id    = sim_ids[ind]
+			sim_entry = filedb.data[sim_id]
+			with bench.show("read"):
+				d  = actdata.read(entry, ["boresight"])
+				d += actdata.read(sim_entry, exclude=["boresight"])
 		with bench.show("calibrate"):
 			d = actdata.calibrate(d, exclude=["autocut"])
 		if d.ndet == 0 or d.nsamp < 2: raise errors.DataMissing("no data in tod")
@@ -61,28 +87,8 @@ for ind in range(comm.rank, len(ids), comm.size):
 		diff = diff[:,:diff.shape[-1]/csize*csize].reshape(d.ndet,-1,csize)
 		ivar = 1/(np.median(np.mean(diff**2,-1),-1)/2**0.5)
 		del diff
-	# Generate planet cut
-	with bench.show("planet cut"):
-		planet_cut = cuts.avoidance_cut(d.boresight, d.point_offset, d.site,
-				args.planet, R)
-	# Subtract atmospheric model
-	with bench.show("atm model"):
-		model = gapfill.gapfill_joneig(tod, planet_cut, inplace=False)
 	# Estimate noise level
 	asens = np.sum(ivar)**-0.5 / d.srate**0.5
-	with bench.show("smooth"):
-		ft   = fft.rfft(model)
-		freq = fft.rfftfreq(model.shape[-1])*d.srate
-		flt  = 1/(1+(freq/model_fknee)**model_alpha)
-		ft  *= flt
-		fft.ifft(ft, model, normalize=True)
-		del ft, flt, freq
-	with bench.show("atm subtract"):
-		tod -= model
-		del model
-		tod  = tod.astype(dtype, copy=False)
-	# Should now be reasonably clean of correlated noise.
-	# Proceed to make simple binned map
 	with bench.show("actscan"):
 		scan = actscan.ACTScan(entry, d=d)
 	with bench.show("pmat"):
@@ -91,6 +97,27 @@ for ind in range(comm.rank, len(ids), comm.size):
 		rhs  = enmap.zeros((ncomp,)+shape, area.wcs, dtype)
 		div  = enmap.zeros((ncomp,ncomp)+shape, area.wcs, dtype)
 		junk = np.zeros(pcut.njunk, dtype)
+	# Generate planet cut
+	with bench.show("planet cut"):
+		planet_cut = cuts.avoidance_cut(d.boresight, d.point_offset, d.site,
+				args.planet, R)
+	if args.sim:
+		if args.noiseless: tod_orig = tod.copy()
+		with bench.show("inject"):
+			pmap.forward(tod, area)
+	# Compute atmospheric model
+	with bench.show("atm model"):
+		model  = smooth(gapfill.gapfill_joneig(tod,      planet_cut, inplace=False), d.srate)
+	if args.sim and args.noiseless:
+		model -= smooth(gapfill.gapfill_joneig(tod_orig, planet_cut, inplace=False), d.srate)
+		tod   -= tod_orig
+		del tod_orig
+	with bench.show("atm subtract"):
+		tod -= model
+		del model
+		tod  = tod.astype(dtype, copy=False)
+	# Should now be reasonably clean of correlated noise.
+	# Proceed to make simple binned map
 	with bench.show("rhs"):
 		tod *= ivar[:,None]
 		pcut.backward(tod, junk)
@@ -112,7 +139,6 @@ for ind in range(comm.rank, len(ids), comm.size):
 	mcent = map[:,c[0]-crad:c[0]+crad,c[1]-crad:c[1]+crad]
 	mcent = enmap.downgrade(mcent, 4)
 	amp   = np.max(mcent)
-	print amp, asens
 	print "%s amp %7.3f asens %7.3f" % (id, amp/1e6, asens)
 	with bench.show("write"):
 		enmap.write_map("%s%s_map.fits" % (prefix, bid), map)

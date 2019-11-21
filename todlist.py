15ca2a4dfa14ef3203e5eda16ce2c1530dd6143d
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2019-11-21 13:30:02 -0500

    Make todlist python3 compatible

--- todlist.py
+++ todlist.py
@@ -1,3 +1,4 @@
+from __future__ import print_function, division
 import os
 from enact import filedb, todinfo
 from enlib import config
@@ -9,4 +10,4 @@ if args.dbfile:
 	filedb.scans = todinfo.read(args.dbfile)
 else:
 	filedb.init()
-print repr(filedb.scans.select(filedb.scans[args.query]))
+print(repr(filedb.scans.select(filedb.scans[args.query])))

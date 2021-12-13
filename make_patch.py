e71e3f6fe1c304ab71fc1e12d533097ab86d7144
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2021-12-13 17:13:16 -0500

    make make_patch conform to other box arguments

--- make_patch.py
+++ make_patch.py
@@ -1,15 +1,12 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("dec1", type=float)
-parser.add_argument("dec2", type=float)
-parser.add_argument("ra1",  type=float)
-parser.add_argument("ra2",  type=float)
+parser.add_argument("box", type=str)
 parser.add_argument("ofile")
 parser.add_argument("-r", "--res",  type=float, default=0.5)
 parser.add_argument("-p", "--proj", type=str, default="car")
 args = parser.parse_args()
 import numpy as np
 from pixell import utils, enmap
-box = np.array([[args.dec1,args.ra1],[args.dec2,args.ra2]])*utils.degree
+box = utils.parse_box(args.box)*utils.degree
 shape, wcs = enmap.geometry(box, res=args.res*utils.arcmin, proj=args.proj)
 enmap.write_map(args.ofile, enmap.zeros(shape, wcs, np.uint8))

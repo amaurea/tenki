5874006f0bd005f516c844ba3bbe559fd20eba67
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2024-06-01 17:12:58 -0400

    Make fejer1 the default variant in make_patch

--- make_patch.py
+++ make_patch.py
@@ -5,13 +5,13 @@ parser.add_argument("ofile")
 parser.add_argument("-r", "--res",  type=float, default=0.5)
 parser.add_argument("-p", "--proj", type=str, default="car")
 parser.add_argument("-f", "--full", action="store_true")
-parser.add_argument("-V", "--variant", type=str, default=None)
+parser.add_argument("-V", "--variant", type=str, default="fejer1")
 args = parser.parse_args()
 import numpy as np
 from pixell import utils, enmap
 box = utils.parse_box(args.box)*utils.degree
 shape, wcs = enmap.geometry(box, res=args.res*utils.arcmin, proj=args.proj)
-if   args.variant is None: pass
+if   args.variant == "cc": pass
 elif args.variant == "fejer1": wcs.wcs.crpix[1] -= 0.5
 else: raise ValueError("Unknown variant '%s'" % str(args.variant))
 if args.full:
